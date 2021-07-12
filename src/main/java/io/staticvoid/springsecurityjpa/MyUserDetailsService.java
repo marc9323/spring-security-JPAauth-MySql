951b1d539ffb51e4088ab4d41a761cb8bcb3ec90
Marc D. Holman <marcholman9323@gmail.com>
2021-07-11 20:30:24 -0500

    revised UserDetails / ..Service

--- src/main/java/io/staticvoid/springsecurityjpa/MyUserDetailsService.java
+++ src/main/java/io/staticvoid/springsecurityjpa/MyUserDetailsService.java
@@ -1,18 +1,31 @@
 package io.staticvoid.springsecurityjpa;
 
 import io.staticvoid.springsecurityjpa.models.MyUserDetails;
+import io.staticvoid.springsecurityjpa.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
         // hard coded user details instance
-        return new MyUserDetails(s);
+       //  return new MyUserDetails(s);
+        Optional<User> user = userRepository.findByUserName(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
+        // convert to type: MyUserDetails
+       return user.map(MyUserDetails::new).get();
+
+
     }
 }
--- src/main/java/io/staticvoid/springsecurityjpa/models/MyUserDetails.java
+++ src/main/java/io/staticvoid/springsecurityjpa/models/MyUserDetails.java
@@ -6,30 +6,40 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
+// whatever User instance is passed it constructs UserDetails
 public class MyUserDetails implements UserDetails {
 
     private String userName;
+    private String password;
+    private boolean active;
+    private List<GrantedAuthority> authorities;
 
-    public MyUserDetails(String userName) {
-        this.userName = userName;
-    }
+//    public MyUserDetails(String userName) {
+//        this.userName = userName;
+//    }
 
-    public MyUserDetails() {
-
+    public MyUserDetails(User user) {
+        // extract the values form the User instance retrieved by the repository and
+        // convert it into the UserDetails instance
+        this.userName = user.getUserName();
+        this.password = user.getPassword();
+        this.active = user.isActive();
+        this.authorities = Arrays.stream(user.getRoles().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // returns a collection of GrantedAuthority instances
-        return Arrays.asList(
-                new SimpleGrantedAuthority("ROLE_USER")
-        );
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return "pass";
+        return password;
     }
 
     @Override
@@ -54,6 +64,49 @@ public class MyUserDetails implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return true;
+        return active;
     }
+
+    //
+//    public MyUserDetails() {
+//
+//    }
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        // returns a collection of GrantedAuthority instances
+//        return Arrays.asList(
+//                new SimpleGrantedAuthority("ROLE_USER")
+//        );
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return "pass";
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return userName;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
 }
